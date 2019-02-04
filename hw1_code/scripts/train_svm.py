57e15ff957b3a866decace8909e4a67be5f113ee
linyiii <chenlinyi1996@yahoo.com>
2019-02-04 14:10:39 -0500

    tuning

--- hw1_code/scripts/train_svm.py
+++ hw1_code/scripts/train_svm.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     # train an svm model and save to output file
     output_file_fd = open(output_file, "w+")
     svm_model = SVC(kernel='linear', class_weight='balanced', cache_size=1000,
-                    C=5)  # tune this!!!!!!!!!!!!!!!!!!!!!!
+                    C=10)  # tune this!!!!!!!!!!!!!!!!!!!!!!
 
     svm_model.fit(feat_matrix, label_vector)
     cPickle.dump(svm_model, output_file_fd)
